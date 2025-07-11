026b4321a8d8a88bc3af4ec073bd05a3aa4d2374
rapsoto007 <rs9614@nyu.edu>
2025-07-11 11:35:41 -0400

    Add files via upload

--- DNSServer.py
+++ DNSServer.py
@@ -35,12 +35,12 @@ def encrypt_with_aes(input_string, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
     encrypted_data = f.encrypt(input_string.encode('utf-8')) #call the Fernet encrypt method
-    return encrypted_data    
+    return encrypted_data.decode('utf-8')    # Return string version for TXT
 
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
+    decrypted_data = f.decrypt(encrypted_data.encode('utf-8')) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
 salt = b'Tandon' # Remember it should be a byte-object
@@ -81,7 +81,7 @@ dns_records = {
     'yahoo.com.': { dns.rdatatype.A: '192.168.1.105' },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: (encrypted_value.decode('utf-8'),),
+        dns.rdatatype.TXT: (encrypted_value,),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
